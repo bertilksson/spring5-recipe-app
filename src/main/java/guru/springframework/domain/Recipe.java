988c72ad7b01cbb5738f570ed223d9c98780edea
Bertil Karlsson <bertil.ksson@telia.com>
2018-05-03 13:37:35 +0200

    working version of recipe app

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,14 @@
+package guru.springframework.domain;
+
+public class Recepie {
+    private String description;
+    private Integer preparationTime;
+    private Integer cookTIme;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    private Byte[] image;
+    private Notes notes;
+}
